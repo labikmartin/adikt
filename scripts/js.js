296eee621f6b96d65ee6276a7414d0629f3b8a85
martinlabik <martin.labik@hotmail.com>
2016-10-24 00:37:22 +0200

    JS done

--- scripts/js.js
+++ scripts/js.js
@@ -2,16 +2,24 @@ $(function() {
 
 /////////////////////////////////////////// VARIABLES
 // elements definition
-  var windowObject = window;
-      html =         document.querySelectorAll('html, body'),
-      services =     document.getElementById('services'),
-      arrow =        document.getElementsByClassName('header__arrow'),
-      navbar =       document.getElementsByClassName('nav')[0],
-      navbarItem =   document.getElementsByClassName('nav__item'),
-      navPointer =   document.getElementsByClassName('nav__anchor'),
-      navbarCols =   document.querySelectorAll('.nav .col-3, .nav .col-6'),
-      navbarLogo =   document.getElementsByClassName('logo img'),
-      submenuLine =  document.getElementsByClassName('submenu__line');
+  var windowObject =    window,
+      html =            document.querySelectorAll('html, body'),
+      services =        document.getElementById('services'),
+      arrow =           document.getElementsByClassName('header__arrow')[0],
+      navbar =          document.getElementsByClassName('nav')[0],
+      navFixed =        document.getElementById('fixed'),
+      navbarItem =      document.getElementsByClassName('nav__item'),
+      navPointer =      document.getElementsByClassName('nav__anchor'),
+      navbarCols =      document.querySelectorAll('.nav .col-3, .nav .col-6'),
+      navbarLogo =      document.querySelector('.nav__logo img'),
+      submenu =         document.getElementsByClassName('submenu')[0],
+      pageSections =    document.querySelectorAll('#services, #members, #contact'),
+      documentHeight,
+      offsets = [];
+
+// lengths
+  var pageSectionsLength =  pageSections.length,
+      navPointerLength =     navPointer.length;
 
 //properties
   var screenSize = windowObject.offsetHeight;
@@ -24,6 +32,62 @@ $(function() {
       black =   '#000';
 
   /////////////////////////////////////////// UTILITY FUNCTIONS
+  // add class
+  function u_addClass(elem, classes) {
+    var classList = elem.className;
+    var update = classList + ' ' + classes;
+    var isUpdated = classList.match(classes);
+    if (!isUpdated) {
+      elem
+        .className = update;
+    }
+    else { return false; }
+  }
+
+  //remove class
+  function u_removeClass(elem, classes) {
+    var matchPattern = new RegExp('\\s?' + classes, 'g');
+    var classList = elem.className;
+    var hasClass = classList.match(classes);
+    if (hasClass) {
+      var removeClasses = classList.toString().replace(matchPattern, '');
+      elem
+        .className = removeClasses;
+    } else { return false; }
+  }
+
+  // -- scroll animation
+  function u_scroll(toElement, speed) {
+    var windowObject =  window,
+        windowPos =     windowObject.pageYOffset,
+        //if null >>> use arrow element
+        pointer =       (toElement.hasAttribute('href')) ? toElement.getAttribute('href').slice(1) : null,
+        elem =          (pointer !== null) ? document.getElementById(pointer) : services,
+        elemOffset =    elem.offsetTop;
+    var counter = setInterval(function() {
+      windowPos;
+      if (windowPos > elemOffset) { //bottom to top
+        windowObject
+          .scrollTo(0, windowPos);
+        windowPos-=speed;
+        if (windowPos <= elemOffset) { //cancel scrolling
+          clearInterval(counter);
+          windowObject
+            .scrollTo(0, elemOffset+1);
+        }
+      } else { //top to bottom
+        windowObject
+          .scrollTo(0, windowPos);
+        windowPos+=speed;
+        if (windowPos >= elemOffset) { // cancel scrolling
+          clearInterval(counter);
+          windowObject
+            .scrollTo(0, elemOffset+2);
+        }
+      }
+    }, 1);
+  }
+
   //make array of multiple elements
   function u_arrayElems() {
     var elements = [],
@@ -39,135 +103,114 @@ $(function() {
       }
     }
     return elements;
-  };
+  }
+
+  //test value of attribute
+  function u_testAttrVal(selector, attr, val) {
+    var element = document.querySelector(selector);
+    var attribute = element.getAttribute(attr);
+    if (attribute != val) {
+      return true;
+    } else {return false;}
+  }
 
 /////////////////////////////////////////// FUNCTIONS
-// -- scroll animation
-function scroll(toElement, speed) {
-  var windowObject =  window,
-      windowPos =     windowObject.pageYOffset,
-      //if null >>> use arrow element
-      pointer =       (toElement.hasAttribute('href')) ? toElement.getAttribute('href').slice(1) : null,
-      elem =          (pointer !== null) ? document.getElementById(pointer) : services,
-      elemOffset =    elem.offsetTop;
-  var counter = setInterval(function() {
-    windowPos;
-    if (windowPos > elemOffset) { //bottom to top
-      windowObject
-        .scrollTo(0, windowPos);
-      windowPos-=speed;
-      if (windowPos <= elemOffset) { //cancel scrolling
-        clearInterval(counter);
-        windowObject
-          .scrollTo(0, elemOffset);
-      }
-    } else { //top to bottom
-      windowObject
-        .scrollTo(0, windowPos);
-      windowPos+=speed;
-      if (windowPos >= elemOffset) { // cancel scrolling
-        clearInterval(counter);
-        windowObject
-          .scrollTo(0, elemOffset);
-      }
-    }
-  }, 1);
-};
-
 // -- navbar switcher
-function navbarSwitch() {
-  if (windowObject.pageYOffset >= services.offsetTop) {
-    navbar
-      .setAttribute('style',  'background-color:' + white +';'+
-                              'color:' + red +';'+
-                              'border-bottom: solid 1px ' + gray);
-    for (i = 0; i < navPointer.length; i++) {
-      navPointer[i]
-        .setAttribute('style', 'color:' + red + ';');
-    }
-    for (i = 0; i < submenuLine.length; i++) {
-      submenuLine[i]
-        .setAttribute('style', 'background-color:' + red +';');
-    }
-  }
-  else {
-    navbar
-      .removeAttribute('style');
-    var elements = u_arrayElems(navPointer, submenuLine);
-    for (i = 0; i < elements.length; i++) {
-      elements[i]
+function navbarSwitch(offsets, documentHeight) {
+  var imgSource,
+      windowOffset = this.pageYOffset,
+      bottomSection =   window.innerHeight + windowOffset;
+  // change navbar item color according to current window section
+  for (i = 0; i < pageSectionsLength; i++) {
+    var section =       pageSections[i].id,
+        link =          document.getElementsByClassName(section)[0],
+        startSection =  pageSections[i].offsetTop < windowOffset,
+        endSection =    windowOffset < (offsets[i+1] || documentHeight);
+    if (startSection && endSection) {
+      // prevent adding styles
+      if (!(link.hasAttribute('style'))) {
+        link
+          .style.color = black;
+      }
+    // fix for bottom section
+    } else if (documentHeight === bottomSection) {
+      navPointer[navPointerLength-2]
+        .removeAttribute('style');
+      navPointer[navPointerLength-1]
+        .style.color = black;
+    } else {
+      link
         .removeAttribute('style');
     }
   }
-};
+  // switch
+  if (windowObject.pageYOffset >= services.offsetTop) {
+    imgSource = 'images/logo-red.png';
+    u_addClass(navFixed, 'navFixed');
+  }
+  else {
+    imgSource = 'images/logo.png';
+    u_removeClass(navFixed, 'navFixed');
+  }
+  // prevent adding attribute
+  var equalAttribute = u_testAttrVal('.nav__logo img', 'src', imgSource);
+  if (equalAttribute) {
+    navbarLogo
+      .setAttribute('src', imgSource);
+  } else {
+    return;
+  }
+}
+
+/////////////////////////////////////////// ARROW ANIMATION
+var arrowAnim = setInterval(function() {
+  arrow
+    .style.transform = arrow.style.transform == 'scale(1.1)' ? 'scale(1.0)' : 'scale(1.1)';
+  arrow
+    .style.transition = 'all .3s linear';
+}, 500);
 
 /////////////////////////////////////////// PAGE SCROLL
-  arrow[0].addEventListener('click', function() {
-    scroll(this, 18);
+  arrow.addEventListener('click', function() {
+    u_scroll(this, 18);
   });
 
   for (i = 0; i < navPointer.length; i++) {
     navPointer[i]
       .addEventListener('click', function(e) {
-        scroll(this, 18);
+        u_scroll(this, 18);
         e.preventDefault();
       });
-  };
+  }
 
 /////////////////////////////////////////// NAVBAR FUNCTIONALITY
-windowObject.addEventListener('load', navbarSwitch);
+windowObject.addEventListener('load', function() {
+  // store full page height
+  documentHeight = html[0].scrollHeight;
+  // store vertical position of page sections
+  for (i = 0; i < pageSectionsLength; i++) {
+    var  offset = pageSections[i].offsetTop;
+    offsets.push(offset);
+  }
+  //navbar switcher
+  navbarSwitch(offsets, documentHeight);
+});
 
-windowObject.addEventListener('scroll', navbarSwitch);
-
-/*
-  $(windowObject).on('scroll', function() {
-
-    if (navbar.offset().top >= screenSize - 72) {
-      navbar.css({
-        'background-color': '#fff',
-        'color': '#e30613',
-        'border-bottom': 'solid 1px #e5e5e5'
-      });
-
-      anchors.css('color', '#e30613');
-
-      navbarItem.addClass('hover');
-
-      if (windowObject.width() > 1080) {
-        navbarCols.css('padding', '25px 40px');
-      } else {
-        navbarCols.css('padding', '30px 10px');
-      }
-
-      navbarLogo.attr('src', 'images/logo-red.png');
-      burger.css('background-color', '#e30613');
-    } else {
-      navbar.removeAttr('style');
-      anchors.removeAttr('style');
-      navbarItem.removeClass('hover');
-      navbarCols.removeAttr('style');
-      navbarLogo.attr('src', 'images/logo.png');
-      burger.removeAttr('style');
-    }
-  });
-
-  $('.links a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 600);
-    return false;
+windowObject.addEventListener('scroll', function() {
+  //navbar switcher
+  navbarSwitch(offsets, documentHeight);
 });
 
 /////////////////////////////////////////// SLICK
 
   $('.slick').slick({
     'autoplay': true,
-    'arrows': false,
-    'dots': true
+    'arrows':   false,
+    'dots':     true
   });
 
-  if ($(windowObject).width() < 560) {
-    $('.slick-prev, .slick-next').hide();
+  if ($(window).width() < 560) {
+    $('.slick-prev, .slick-next')
+      .hide();
   }
-*/
 });
